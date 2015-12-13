e6f9c9df80b0f10df128283bebbd082ad18f4143
Andrew Shvetsov <mind3ru@gmail.com>
2015-12-13 19:41:00 +0300

    edit messages

--- src/AppBundle/Form/Type/ContactType.php
+++ src/AppBundle/Form/Type/ContactType.php
@@ -14,16 +14,24 @@ class ContactType extends AbstractType
     {
         $builder->add('email',TextType::class, array(
             'constraints' => [
-                new NotBlank(['message'=>'%s не должно быть пустым']),
+                new NotBlank(['message'=>'Email не должен быть пустым']),
                 new Email(),
                 new Length(['max'=>255,'maxMessage'=>'Email'])
             ],
+            'label' => false,
+            'attr' => array(
+                'placeholder' => 'Email',
+            ),
         ))
         ->add('firstName',TextType::class, array(
             'constraints' => [
-                new NotBlank(['message'=>'%s не должно быть пустым']),
+                new NotBlank(['message'=>'Имя не должно быть пустым']),
                 new Length(['max'=>255])
-            ]
+            ],
+            'label' => false,
+            'attr' => array(
+                'placeholder' => 'Имя',
+            ),
         ));
     }
 
